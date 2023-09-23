464d1894ffa76ada8e2e501a0ddc3496a7d96cf2
Wyatt Hooper <wyhoop@cs.csis.work>
2023-09-23 10:35:22 -0500

    Added the first change/implementation of the add score function in scoreboard.cpp

--- scoreboard.cpp
+++ scoreboard.cpp
@@ -4,6 +4,11 @@ Scoreboard::Scoreboard() : totalScore(0) {}
 
 void Scoreboard::addScore(const char* teamMemeber, int score){
 	//Implement adding score here
+	//Wyatt added to this section
+	
+	cout << teamMemeber << " scored " << score << " points!" << endl;
+
+	totalScore += score;
 }
 
 int Scoreboard::getTotalScore() const {
