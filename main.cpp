f6d4964ac4c5cdd12ba7f85bba8c7475290ec06c
Braden Harris <brharr@cs.csis.work>
2023-09-23 09:14:50 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "scoreboard.h"
+
+int main(){
+	Scoreboard scoreboard;
+
+	//Implement your menu-driven program here
+	//Prompt the user to add score and display the total score
+	
+	return 0;
+}
---
+++ scoreboard.cpp
@@ -0,0 +1,11 @@
+#include "scoreboard.h"
+
+Scoreboard::Scoreboard() : totalScore(0) {}
+
+void Scoreboard::addScore(const char* teamMemeber, int score){
+	//Implement adding score here
+}
+
+int Scoreboard::getTotalScore() const {
+	return totalScore;
+}
