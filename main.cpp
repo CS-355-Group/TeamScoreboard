95ffee5efa1c41f102b571cb707183ababe384c3
singladaivik <143966586+singladaivik@users.noreply.github.com>
2023-09-26 17:27:43 -0500

    Two scores added in main for A and B

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@ int main(){
 
 	//Implement your menu-driven program here
 	//Prompt the user to add score and display the total score
+
+	scoreboard.addScore("A", 50);
+	scoreboard.addScore("B", 75);
 	
 	return 0;
 }
